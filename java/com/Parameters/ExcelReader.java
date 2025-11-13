9d188d38245954b3fbfb52e10e0c4143889bf05a
KarriVisalakshi <karri-visalakshi.karri-visalakshi@capgemini.com>
2025-11-13 17:36:59 +0530

    Top Deals code - third commit 5
    Automation Testing

---
+++ java/com/Parameters/ExcelReader.java
@@ -0,0 +1,37 @@
+package com.Parameters;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelReader {
+	
+	public static String[] getRowData(int sheetNo, int rowNum) throws IOException {
+	    String filePath = "src/test/resources/ExcelData/Top Deals - Scenario Outline.xlsx";
+	    FileInputStream file = new FileInputStream(filePath);
+	    Workbook workbook = new XSSFWorkbook(file);
+	    Sheet sheet = workbook.getSheetAt(sheetNo);
+	    DataFormatter formatter = new DataFormatter();
+
+	    // Get header row to determine column count
+	    Row headerRow = sheet.getRow(0);
+	    int colCount = headerRow.getLastCellNum();
+
+	    // Read target row
+	    Row row = sheet.getRow(rowNum);
+	    String[] rowData = new String[colCount];
+	    for (int j = 0; j < colCount; j++) {
+	        Cell cell = (row != null) ? row.getCell(j) : null;
+	        rowData[j] = (cell != null) ? formatter.formatCellValue(cell) : "";
+	    }
+
+	    workbook.close();
+	    file.close();
+	    return rowData;
+	}
+
+	
+}
+
+
